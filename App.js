dbd2d3316c49f7cfcfb95c503de8d7532d1c70c9
rafaell <rafael.a.b.lanfredi@gmail.com>
2021-11-30 18:41:22 -0600

    Adding code comments and parameters validation

--- App.js
+++ App.js
@@ -3,26 +3,47 @@ new Component('ComponentName', {
   template,
   styles,
   props,
-  setup,
   data,
   methods,
   watchers,
+  setup,
+  mounted,
 });
 */
 
-// TODO - HTMLless components
-// TODO - implement watchers attached to the pubsub
-// TODO - bind styles
-
-function App() {
+/**
+ * Class for the App. The app represents the main component of the application.
+ * It hold references to :
+ *   - Component classes
+ *   - Component instances
+ *   - libraries
+ *   - data stores
+ *   - global pubsub
+ * @constructor
+ * @param {function} boot Function to run during app creation. Use it to create libraries, stores and other variables that should be available to all components
+**/
+function App(boot) {
   this.$version = '0.0.29';
   this._componentTypes = {};
   this._components = [];
   this.$libraries = {};
   this.$stores = {};
   this.$pubSub = new PubSub();
+  
+  if(!(boot instanceof Function)) throw new Error(`Expected "boot" of type Function, but found ${boot.cobstructor.name}`);
+
+  boot();
 }
 
+/*
+  Create components for the application. A component can have the following properties:
+- methods: functionsasigned to the component's prototype. They are available both during setup and after mounted.
+    > attribute: method name
+    > value: function
+- watchers: subscribe to events in the pubsub obj. 
+    > attribute: event to subscribe in the pubsub
+    > value: function that will be called when the event is published
+*/
 App.prototype.createComponent = function(
 name, 
 {
@@ -211,14 +232,21 @@ name,
   Component.prototype.styles = styles;
   Component.prototype.setup = setup || this.NOOP;
   
+  // assign each method function to the component's prototype
   for(let method in methods) {
+      if(!(methods[method] instanceof Function)) throw new Error(`Expected method of type Function, but ${method} is of type ${methods[method].constructor.name}.`);
     Component.prototype[method] = methods[method];
   }
   
+  // subscribe to events from the app pubsub
+  // The event is the name of the watcher attribute
   for(let watcher in watchers) {
+      if(!(watchers[watcher] instanceof Function)) throw new Error(`Expected watcher of type Function, but ${watcher} is of type ${watchers[watcher].constructor.name}.`);
+      
       this.$pubSub.subscribe(watcher, watchers[watcher]);
   }
   
+  // if no mountedtmethod was passed, assign an empty function
   Component.prototype.mounted = mounted || this.NOOP;
   
   this._componentTypes[name] = Component;
@@ -226,6 +254,10 @@ name,
 
 App.prototype.NOOP = function () {};
 
+/**
+ * Search for components and build them. The function runs recursively, so it also builds components specified inside the components' template.
+  * @param {HTMLComponent} rootElement The source element where to search for HTML elements bound to components
+**/
 App.prototype.buildComponents = function(rootElement) {
     const components = rootElement.querySelectorAll('[data-component]');
     if(components.length === 0) return;
@@ -244,7 +276,6 @@ App.prototype.buildComponents = function(rootElement) {
   });
 }
     
-
 App.prototype.start = function() {
     this.buildComponents(document);
 }
